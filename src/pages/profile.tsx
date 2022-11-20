af3f10f9a434e7e7ad54b933a9ece5d6771118f1
Mackie <masaki.shimadu@gmail.com>
2022-11-20 20:32:24 +0900

    fix: delete unnecessary file

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -2,14 +2,12 @@ import type { NextPage } from "next";
 import { CommingSoon } from "../components/ComingSoon";
 import { HeroOthers } from "../components/HeroOthers";
 import { Page } from "../components/Page";
-import { SalesCondition } from "../components/SalesCondition";
 
 const profile: NextPage = () => {
   return (
     <Page>
       <HeroOthers>Profile</HeroOthers>
       <CommingSoon />
-      <SalesCondition />
     </Page>
   );
 };
