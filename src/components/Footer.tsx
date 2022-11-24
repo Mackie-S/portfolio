57efdeaaca2de2e80dca4eecad981d589b9fccec
Mackie <masaki.shimadu@gmail.com>
2022-11-25 07:44:09 +0900

    refactor: adjust

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,11 @@
-import { HStack, Box, Heading } from "@chakra-ui/react";
+import {  Box, Heading, Flex } from "@chakra-ui/react";
 import Image from "next/image";
 import Link from "next/link";
 
 export const Footer = () => {
   return (
-    <Box bgColor="brand.gray" w="full" height="150px" padding="20px" display="flex" flexDirection="column">
-      <HStack gap="45px">
+    <Flex bgColor="brand.gray" w="full" height="150px" padding="20px" direction="column">
+      <Flex gap="45px">
         <Link href="/">
           <a>
             <Image src="/images/logo_white.svg" alt="サイトロゴ" width="30px" height="30px" />
@@ -21,10 +21,10 @@ export const Footer = () => {
             <Image src="/images/github_icon.svg" alt="サイトロゴ" width="30px" height="30px" />
           </a>
         </Link>
-      </HStack>
+      </Flex>
       <Heading as="h2" size="md" color="white" alignSelf="center" justifySelf="center">
-        Copyright ©️ 2022 Mackie Lab
+        Copyright ©️ 2022 DaDa Lab
       </Heading>
-    </Box>
+    </Flex>
   );
 };
