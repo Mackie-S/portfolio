648417ce0e0674c4a5e407da88dc0688d4cced04
Mackie <masaki.shimadu@gmail.com>
2022-12-06 15:27:53 +0900

    feat: create it for SP & Tablet

---
+++ src/components/MenuButton.tsx
@@ -0,0 +1,40 @@
+import { Menu, MenuList, MenuItem, MenuButton as ChakraMenuButton, IconButton, Box } from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+type NavMenu = {
+  name: string;
+  link: string;
+};
+
+type MenuButtonProps = {
+  navMenus: NavMenu[];
+};
+
+export const MenuButton = ({ navMenus }: MenuButtonProps) => {
+  const router = useRouter();
+  return (
+    <Box display={["block", "block", "none"]}>
+      <Menu>
+        <ChakraMenuButton as={IconButton} aria-label="Options" icon={<HamburgerIcon />} variant="outline" />
+        <MenuList>
+          {navMenus.map((value, index) => {
+            return (
+              <Link key={index} href={value.link}>
+                <MenuItem
+                  fontWeight="bold"
+                  position="relative"
+                  display="inline-block"
+                  color={router.pathname === value.link ? "brand.gray" : "black"}
+                >
+                  {value.name}
+                </MenuItem>
+              </Link>
+            );
+          })}
+        </MenuList>
+      </Menu>
+    </Box>
+  );
+};
