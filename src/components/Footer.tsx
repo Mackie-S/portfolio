1a7754e830af93fcd54b32784b4d54eb179795c7
Mackie <masaki.shimadu@gmail.com>
2022-12-07 15:26:40 +0900

    fix: implement responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,28 +1,51 @@
-import { Heading, Flex } from "@chakra-ui/react";
-import Image from "next/image";
+import { Heading, Flex, Image as ChakraImage } from "@chakra-ui/react";
+// import Image from "next/image";
 import Link from "next/link";
 
 export const Footer = () => {
   return (
-    <Flex bgColor="brand.gray" w="full" height="150px" padding="20px" direction="column">
-      <Flex gap="45px">
+    <Flex
+      bgColor="brand.gray"
+      w="full"
+      h="150px"
+      padding="20px"
+      direction="column"
+      justify="center"
+      position="relative"
+    >
+      <Flex gap={["20px","20px","45px"]} position="absolute" top="20px">
         <Link href="/">
           <a>
-            <Image src="/images/logo_white.svg" alt="サイトロゴ" width="30px" height="30px" />
+            <ChakraImage
+              src="/images/logo_white.svg"
+              alt="サイトロゴ"
+              w={["20px", "20px", "30px"]}
+              h={["20px", "20px", "30px"]}
+            />
           </a>
         </Link>
         <Link href="https://twitter.com/mackie_eng_web">
           <a target="_blank" rel="noopener">
-            <Image src="/images/twitter_icon.svg" alt="サイトロゴ" width="30px" height="30px" />
+            <ChakraImage
+              src="/images/twitter_icon.svg"
+              alt="サイトロゴ"
+              w={["20px", "20px", "30px"]}
+              h={["20px", "20px", "30px"]}
+            />
           </a>
         </Link>{" "}
         <Link href="https://github.com/Mackie-S">
           <a target="_blank" rel="noopener">
-            <Image src="/images/github_icon.svg" alt="サイトロゴ" width="30px" height="30px" />
+            <ChakraImage
+              src="/images/github_icon.svg"
+              alt="サイトロゴ"
+              w={["20px", "20px", "30px"]}
+              h={["20px", "20px", "30px"]}
+            />
           </a>
         </Link>
       </Flex>
-      <Heading as="h2" size="md" color="white" alignSelf="center" justifySelf="center">
+      <Heading as="h2" size={["sm", "sm", "md"]} color="white" alignSelf="center">
         Copyright ©️ 2022 DaDa Lab
       </Heading>
     </Flex>
