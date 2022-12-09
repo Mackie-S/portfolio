59ab64209f866f83acac0931679491b0b8594558
Mackie <masaki.shimadu@gmail.com>
2022-12-09 16:05:25 +0900

    fix: modify layout

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,20 +1,44 @@
-import { Box, UnorderedList, ListItem } from "@chakra-ui/react";
+import { Flex, Heading, List, ListItem } from "@chakra-ui/react";
 import { Link } from "@chakra-ui/react";
+import type { Category } from "../types/blog";
 
-export const Categories = ({ categories }: any) => {
+type Props = {
+  categories: Category[];
+};
+
+export const Categories = ({ categories }: Props) => {
   return (
-    <Box>
-      <UnorderedList>
-        {categories.map((category: any) => {
+    <Flex px={10} gap={4} direction="column" maxW="400px">
+      <Heading as="h3" size="md" bgColor="#eee" py="6px" px="10px" borderRadius="5px">
+        カテゴリー
+      </Heading>
+      <List>
+        {categories.map((category) => {
           return (
-            <ListItem key={category.id}>
-              <Link href={`/category/${category.id}`}>
+            <Link key={category.id} href={`/category/${category.id}`}>
+              <ListItem
+                mb={5}
+                py="6px"
+                px="10px"
+                position="relative"
+                _after={{
+                  content: '""',
+                  display: "inline-block",
+                  width: "100%",
+                  height: "1px",
+                  position: "absolute",
+                  borderRadius: "1px",
+                  backgroundColor: "#eee",
+                  bottom: "-3px",
+                  left: "0",
+                }}
+              >
                 {category.name}
-              </Link>
-            </ListItem> 
+              </ListItem>
+            </Link>
           );
         })}
-      </UnorderedList>
-    </Box>
+      </List>
+    </Flex>
   );
 };
